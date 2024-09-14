85851dfc4503d0de67a3cc05077946695cc39297
maxbiletskyi1 <biletskyimaxacc@gmail.com>
2024-09-14 11:00:31 +0200

    Top Up Weapon

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,9 @@
 Changes made.
-I'm not the author of the mod.
+Author: https://github.com/Zenbeau/ImGuiRedux_Cleo_GTA_SA_DE
 I'm the author of the update.
+
+Update:
+Topping up the weapon held by player by 500
+
+Removed:
+Topping up incomlete list of weapons in the invetory
--- cheat-menu/functions/weapon.ts
+++ cheat-menu/functions/weapon.ts
@@ -3,13 +3,7 @@ import { WeaponType } from '../../.config/enums';
 
 import { INFINITE_WEAPON_AMMO } from '../data/index';
 
-export const infiniteAmmo = (char: Char): Char =>
+export const infiniteAmmo = (char: Char): Char => 
+    char.setAmmo(char.getCurrentWeapon(), char.getAmmoInWeapon(char.getCurrentWeapon())+INFINITE_WEAPON_AMMO);
+
     
-    char.setAmmo(WeaponType.Pistol, char.getAmmoInWeapon(WeaponType.Pistol)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.Shotgun, char.getAmmoInWeapon(WeaponType.Shotgun)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.Mp5, char.getAmmoInWeapon(WeaponType.Mp5)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.M4, char.getAmmoInWeapon(WeaponType.M4)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.Sniper, char.getAmmoInWeapon(WeaponType.Sniper)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.RocketLauncher, char.getAmmoInWeapon(WeaponType.RocketLauncher)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.Satchel, char.getAmmoInWeapon(WeaponType.Satchel)+INFINITE_WEAPON_AMMO)
-        .setAmmo(WeaponType.SprayCan, char.getAmmoInWeapon(WeaponType.SprayCan)+INFINITE_WEAPON_AMMO);
